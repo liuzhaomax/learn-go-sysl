671c88255cac7e3da3d93066a4ee081bcb81f336
LIU Zhao <liuzhaomax@163.com>
2022-09-07 00:14:00 +1000

    first api done

--- cmd/Petdemo/main.go
+++ cmd/Petdemo/main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-    "context"
+	"context"
+	"github.com/anz-bank/sysl-go-demo/src/handlers"
 
-    "github.com/anz-bank/sysl-go/core"
+	"github.com/anz-bank/sysl-go/core"
 
-    "github.com/anz-bank/sysl-go-demo/internal/gen/pkg/servers/Petdemo"
+	"github.com/anz-bank/sysl-go-demo/internal/gen/pkg/servers/Petdemo"
 )
 
 type AppConfig struct {
-    // Define app-level config fields here.
+	// Define app-level config fields here.
 }
 
 func main() {
-    petdemo.Serve(context.Background(),
-        func(ctx context.Context, config AppConfig) (*petdemo.ServiceInterface, *core.Hooks, error) {
-            // Perform one-time setup based on config here.
-            return &petdemo.ServiceInterface{
-                // Add handlers here.
-            }, nil, nil
-        },
-    )
+	petdemo.Serve(context.Background(),
+		func(ctx context.Context, config AppConfig) (*petdemo.ServiceInterface, *core.Hooks, error) {
+			// Perform one-time setup based on config here.
+			return &petdemo.ServiceInterface{
+				// Add handlers here.
+				GetPetList: handlers.GetRandomPetPicListRead,
+			}, nil, nil
+		},
+	)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/anz-bank/sysl-go-demo
 
 go 1.16
 
-require github.com/anz-bank/sysl-go v0.269.0 // indirect
+require (
+	github.com/anz-bank/sysl-go v0.269.0
+	github.com/go-chi/chi v4.1.2+incompatible
+	github.com/stretchr/testify v1.7.0
+)
