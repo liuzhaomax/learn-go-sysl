b6f7364d9fefe3612bb18c6aaf3f5779166ef6c9
LIU Zhao <liuzhaomax@163.com>
2022-09-07 00:02:56 +1000

    go1.16

---
+++ Makefile
@@ -0,0 +1,14 @@
+SYSLGO_SYSL=specs/petdemo.sysl
+SYSLGO_PACKAGES=Petdemo
+SYSLGO_APP.Petdemo = Petdemo
+
+-include local.mk
+include codegen.mk
+
+.PHONY: clean
+clean:
+	rm -rf internal/gen
+
+.PHONY: test
+test: gen-all-servers
+	go test -v ./...
---
+++ cmd/Petdemo/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "context"
+
+    "github.com/anz-bank/sysl-go/core"
+
+    "github.com/anz-bank/sysl-go-demo/internal/gen/pkg/servers/Petdemo"
+)
+
+type AppConfig struct {
+    // Define app-level config fields here.
+}
+
+func main() {
+    petdemo.Serve(context.Background(),
+        func(ctx context.Context, config AppConfig) (*petdemo.ServiceInterface, *core.Hooks, error) {
+            // Perform one-time setup based on config here.
+            return &petdemo.ServiceInterface{
+                // Add handlers here.
+            }, nil, nil
+        },
+    )
+}
